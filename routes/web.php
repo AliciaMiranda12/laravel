115fa61cb46e55c7269f7a973ad2e2f4f8f7a84e
wns-208 <lordwillias@gmail.com>
2026-03-17 14:49:50 -0300

    ss

--- routes/web.php
+++ routes/web.php
@@ -12,14 +12,9 @@ use App\Http\Middleware\LogAcessoMiddleware;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::prefix('publico')->group(function(){
+
 
     Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
-    Route::get('/contato/{nome}', [App\Http\Controllers\Principal::class, 'contato']);
-    Route::get('/contato/{nome}/{sobrenome}', [App\Http\Controllers\Principal::class, 'contatoNomeCompleto']);
-    Route::get('/contato/{nome}/{sobrenome}/{mensagem}', [App\Http\Controllers\Principal::class, 'contatoMensagem']);
-    Route::get('/contato/{nome}/{sobrenome}/{mensagem}/{telefone}/{email?}', [App\Http\Controllers\Principal::class, 'contatoTelefone']);
-});
 
 
 
