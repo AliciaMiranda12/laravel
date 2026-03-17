635c0eaef2999f8f49dcfcdfe43d24ea54245889
AliciaMiranda12 <mirandacaly@gmail.com>
2026-03-17 14:47:39 -0300

    aula 17/03

--- app/Http/Controllers/Principal.php
+++ app/Http/Controllers/Principal.php
@@ -6,11 +6,31 @@ use Illuminate\Http\Request;
 
 class Principal extends Controller
 {
-    function principal(){
-        echo 'Página Principal';
-    }
+        function principal(){
+            echo 'Página Principal';
+        }
 
-    
+        function contato(string $nome){
+            echo $nome;
+        }
+
+        function contatoNomeCompleto (string $nome, string $sobrenome){
+            echo "O contato é $nome $sobrenome";
+        }
+
+        function contatoMensagem (string $nome, string $sobrenome, string $mensagem){
+            echo "O contato $nome $sobrenome enviou uma mensagem";
+            echo "<h1>$mensagem</h1>";
+        }
+
+        function contatoTelefone (string $nome, string $sobrenome, string $mensagem, string $telefone, string $email = 'E-mail não informado'){
+            echo "O contato $nome $sobrenome enviou uma mensagem";
+            echo "<h1>$mensagem</h1>";
+            echo "Telefone: $telefone";
+            echo "<br>Email: $email</br>";
+            
+        }
+
 }
 
 
